7930791bd98ba66ca2005a22cc53e33ed5ad2ef9
alexlockhart7 <alex.lockhart@icfolson.com>
2018-02-26 08:00:43 -0500

    Reimplemented aloc-button in a cleaner way while also removing the need for hyperHTML.

--- public/components/aloc-button.js
+++ public/components/aloc-button.js
@@ -1,46 +1,58 @@
-import hyper from "./../vendor/hyperhtml/esm/index.js";
+const template = document.createElement('template');
+template.innerHTML = `
+  <style>
+    :host {
+      display: block;
+      text-align: center;
+      background: #039be5;
+      text-decoration: none;
+      color: white;
+      padding: 16px;
+      border-radius: 4px;
+      box-shadow: 0 1px 3px rgba(0,0,0,0.12), 0 1px 2px rgba(0,0,0,0.24);
+      cursor: pointer;
+    }
+  </style>
+  <slot></slot>
+`;
 
 export default class AlocButton extends HTMLElement {
   static get is() {
-    return "aloc-button";
+    return 'aloc-button';
   }
 
-  get href() { return this._href }
-  set href(href) { this._href = href; this.render(); }
+  static get observedAttributes() {
+    return ['href'];
+  }
+
+  get href() { return this.getAttribute('href') }
+  set href(href) {
+    var hasHref = Boolean(href);
+    if (href) {
+      this.setAttribute('href', href);
+    } else {
+      this.removeAttribute('href');
+    }
+  }
 
   constructor() {
     super();
     this.attachShadow({mode: 'open'});
-    this.shadowHyper = hyper.bind(this.shadowRoot);
-
-    this._href = this.getAttribute("href");
+    this.shadowRoot.appendChild(template.content.cloneNode(true));
   }
 
   connectedCallback() {
-    this.render();
+    this.addEventListener('click', this._onClick);
   }
 
-  render() {
-    this.shadowHyper`
-      ${this._styles()}
-      <a href=${this.href}><slot></slot></a>
-    `
+  disconnectedCallback() {
+    this.removeEventListener('click', this._onClick);
   }
 
-  _styles() {
-    return hyper`<style>
-      a {
-        display: block;
-        text-align: center;
-        background: #039be5;
-        text-decoration: none;
-        color: white;
-        padding: 16px;
-        border-radius: 4px;
-        box-shadow: 0 1px 3px rgba(0,0,0,0.12), 0 1px 2px rgba(0,0,0,0.24);
-        cursor: pointer;
-      }
-    </style>`;
+  _onClick(event) {
+    if (this.href) {
+      window.location = this.href
+    }
   }
 }
 
